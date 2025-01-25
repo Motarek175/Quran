e1dcf1950a493a2d3b51047f29bc896dcb23b214
mohaed tarek <fcb6480@gmail.com>
2025-01-26 01:24:27 +0200

    Prepare for deployment

--- src/app/Components/surah/surah.component.ts
+++ src/app/Components/surah/surah.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, RouterLink, RouterLinkActive } from '@angular/router';
 import { GetDataService } from '../../get-data.service';
 import { CommonModule } from '@angular/common';
-
 @Component({
   selector: 'app-surah',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink, RouterLinkActive],
   templateUrl: './surah.component.html',
   styleUrl: './surah.component.css',
 })
@@ -14,37 +13,35 @@ export class SurahComponent {
   id: number = 0;
   surahsName: any;
   selectedSurah: any;
+
   constructor(
     private _ActivatedRoute: ActivatedRoute,
     private GetDataService: GetDataService
   ) {
-    _ActivatedRoute.paramMap.subscribe((params) => {
+    this._ActivatedRoute.paramMap.subscribe((params) => {
       this.id = parseInt(params.get('id')!);
-      this.id--;
+      this.loadSurahData();
     });
   }
   ngOnInit(): void {
     this.GetDataService.getSurah().subscribe({
       next: (res) => {
         this.surahsName = res.data.surahs;
-        this.selectedSurah = this.surahsName[this.id];
-        for (const ayah of this.selectedSurah.ayahs) {
-          let state: string = ayah.text;
-          if (state.includes('بِسْمِ ٱللَّهِ ٱلرَّحْمَٰنِ ٱلرَّحِيمِ')) {
-            ayah.text = state.replace(
-              'بِسْمِ ٱللَّهِ ٱلرَّحْمَٰنِ ٱلرَّحِيمِ',
-              ''
-            );
-          }
-          if (state.includes('1')) {
-            ayah.text = state.replace('1', '');
-          }
-        }
-
+        this.loadSurahData();
       },
       error: (error) => {
-        console.log(error);
+        console.error(error);
       },
     });
+  }
+  loadSurahData(): void {
+    this.selectedSurah = this.surahsName[this.id - 1];
+    console.log(this.selectedSurah);
+    for (const ayah of this.selectedSurah?.ayahs || []) {
+      let state: string = ayah.text;
+      if (state.includes('بِسْمِ ٱللَّهِ ٱلرَّحْمَٰنِ ٱلرَّحِيمِ')) {
+        ayah.text = state.replace('بِسْمِ ٱللَّهِ ٱلرَّحْمَٰنِ ٱلرَّحِيمِ', '');
+      }
+    }
   }
 }
