78bc03fb011662f2816d919e09ca5c68046471ae
mohaed tarek <fcb6480@gmail.com>
2025-02-17 11:03:02 +0200

    Prepare for deployment

--- src/app/Components/home/home.component.ts
+++ src/app/Components/home/home.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { GetDataService } from '../../get-data.service';
 import { CommonModule } from '@angular/common';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { load } from '../../interceptors/loader-interceptor.service';
 
 @Component({
   selector: 'app-home',
@@ -13,7 +14,11 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
 export class HomeComponent {
   constructor(public GetDataService: GetDataService) {}
   surahsName: any;
+  isLoading = false;
   ngOnInit(): void {
+    load.subscribe((status) => {
+      this.isLoading = status;
+    });
     this.GetDataService.getSurah().subscribe({
       next: (res) => {
         this.surahsName = res.data.surahs;
--- src/app/get-data.service.ts
+++ src/app/get-data.service.ts
@@ -12,14 +12,7 @@ export class GetDataService {
   getSurah(): Observable<any> {
     return this.http.get('https://api.alquran.cloud/v1/quran/quran-uthmani');
   }
-  // getFullAudio(num: number): Observable<any> {
-  //   const formattedNum = num.toString().padStart(3, '0');
-  //   return this.http.get(
-  //     'http://download.quranicaudio.com/quran/ahmed_ibn_3ali_al-3ajamy/' +
-  //       formattedNum +
-  //       '.mp3'
-  //   );
-  // }
+
   getFullAudioUrl(surahNumber: number): string {
     const formattedNumber = surahNumber.toString().padStart(3, '0'); // Format to 3 digits
     return `https://download.quranicaudio.com/quran/ahmed_ibn_3ali_al-3ajamy/${formattedNumber}.mp3`;
